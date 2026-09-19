3fcbb9627f5e365e3fdfff075e0831fcd8ae475e
yanpeng00826-bit <yanpeng00826@gmail.com>
2026-09-19 18:29:14 +0800

    发布朝阳英语点读网页版

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();await self.clients.claim()})()));
